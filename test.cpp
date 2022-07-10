170e809d8c9f1092cb88784b57a01f552c32f965
beyanon <tokabe333@gmail.com>
2022-07-10 15:51:16 +0900

    AB OK

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+const double PI = 3.141592653589793;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long int ll;
+using namespace std;
+
+int main() {
+  cout << -15 % 10 << endl;
+
+  return 0;
+}
