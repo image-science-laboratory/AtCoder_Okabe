510e3eff72f3703f6ad994223cf1478cc4cf136f
beyanon <tokabe333@gmail.com>
2022-07-10 13:39:28 +0900

    Update B-CounterclockwiseRotation.cpp

--- ABC_250to300/ABC_259/B-CounterclockwiseRotation.cpp
+++ ABC_250to300/ABC_259/B-CounterclockwiseRotation.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+const double PI = 3.14159265358979;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 typedef long long int ll;
 using namespace std;
@@ -14,7 +15,9 @@ int main() {
   cin >> a >> b >> d;
 
   double th = atan2(b, a);
-  th = (th + d / 180.0 * M_PI) % (2 * M_PI);
+  th = fmod(th + d / 180.0 * PI, 2 * PI);
+  double r = sqrt(a * a + b * b);
 
+  cout << r * cos(th) << " " << r * sin(th) << endl;
   return 0;
 }
