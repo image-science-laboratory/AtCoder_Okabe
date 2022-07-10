9dde28506a7cb2771a83422c9027e4756d91d9fc
beyanon <tokabe333@gmail.com>
2022-07-10 16:13:49 +0900

    Update C-Rotation.cpp

--- ABC_250to300/ABC_258/C-Rotation.cpp
+++ ABC_250to300/ABC_258/C-Rotation.cpp
@@ -12,58 +12,25 @@ const double PI = 3.141592653589793;
 typedef long long int ll;
 using namespace std;
 
-class RingBuffer {
- public:
-  ll max_size = 100000000;  // 10^8
-  ll* buff;
-  ll top = 0;
-  ll bottom = 0;
-  ll size = 0;
-
-  RingBuffer() { buff = new ll[max_size]; }
-  RingBuffer(ll n) {
-    buff = new ll[n];
-    max_size = n;
-  }
-
-  void push_back(ll n) {
-    bottom = (bottom + 1) % max_size;
-    buff[bottom] = n;
-    size += 1;
-  }
-
-  void push_front(ll n) {
-    top = (top - 1) % max_size;
-    if (top < 0) top = max_size + top;
-    buff[top] = n;
-    size += 1;
-  }
-
-  ll pop_back() {
-    ll ret = buff[bottom];
-    bottom = (bottom - 1) % max_size;
-    if (bottom < 0) bottom = max_size + bottom;
-    size -= 1;
-    return ret;
-  }
-
-  ll pop_front() {
-    ll ret = buff[top];
-    top = (top + 1) % max_size;
-    size -= 1;
-    return ret;
-  }
-
-  ll back() { return buff[bottom]; }
-
-  ll front() { return buff[top]; }
-}
-
 int main() {
   int n, q;
   cin >> n >> q;
   string s;
   cin >> s;
+
+  ll cnt = 0;
+  rep(i, q) {
+    int p, x;
+    cin >> p >> x;
+    if (p == 2) {
+      x -= 1;
+      x = (x - cnt) % n;
+      if (x < 0) x = n + x;
+      cout << s[x] << endl;
+    } else {
+      cnt = (cnt + x) % n;
+    }
+  }
 
   return 0;
 }
