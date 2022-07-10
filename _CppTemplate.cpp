5efcde8c861b0d578266f8985e4cb3dd2b8eaa60
beyanon <tokabe333@gmail.com>
2022-07-10 13:47:14 +0900

    Update _CppTemplate.cpp

--- _CppTemplate.cpp
+++ _CppTemplate.cpp
@@ -1,12 +1,14 @@
 #include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <queue>
 #include <string>
 #include <unordered_map>
 #include <vector>
+const double PI = 3.141592653589793;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-const double PI = 3.14159265358979;
 typedef long long int ll;
 using namespace std;
 
