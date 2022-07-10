ccb5329a653c724bbdafabe03500e4e648848832
beyanon <tokabe333@gmail.com>
2022-07-10 13:34:47 +0900

    Update _CppTemplate.cpp

--- _CppTemplate.cpp
+++ _CppTemplate.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <vector>
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+const double PI = 3.14159265358979;
 typedef long long int ll;
 using namespace std;
 
