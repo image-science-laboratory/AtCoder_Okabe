9f1907cf916c17f1f36d0f9d1bb9477299816fd1
beyanon <tokabe333@gmail.com>
2022-07-10 16:48:25 +0900

    Update : include 追加

--- ABC_250to300/ABC_257/B-1DPawn.cpp
+++ ABC_250to300/ABC_257/B-1DPawn.cpp
@@ -14,7 +14,24 @@ typedef long long int ll;
 using namespace std;
 
 int main() {
-  int n;
+  int n, k, q;
+  cin >> n >> k >> q;
+
+  vector<int> arr(k);
+  rep(i, k) scanf("%d", &arr[i]);
+  sort(arr.begin(), arr.end());
+
+  rep(i, q) {
+    int l;
+    scanf("%d", &l);
+    l -= 1;
+
+    if (l == n - 1 || arr[l] + 1 == arr[l + 1]) continue;
+    arr[l] += 1;
+  }
+
+  rep(i, k) cout << arr[i] << " ";
+  cout << endl;
 
   return 0;
 }
