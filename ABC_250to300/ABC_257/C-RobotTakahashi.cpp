9db5106a3898ece6c7a70b3b3785626d756478fe
beyanon <tokabe333@gmail.com>
2022-07-11 15:05:35 +0900

    Update C-RobotTakahashi.cpp

--- ABC_250to300/ABC_257/C-RobotTakahashi.cpp
+++ ABC_250to300/ABC_257/C-RobotTakahashi.cpp
@@ -14,46 +14,34 @@ typedef long long int ll;
 using namespace std;
 
 int main() {
-  int n;
+  int n, oya = 0;
+  ll tmp;
   string s;
   cin >> n >> s;
 
   vector<pair<ll, int>> arr(n);
-  unordered_map<ll, pair<int, int>> hash;
   rep(i, n) {
-    ll w;
-    scanf("%lld", &w);
-    int pc = -1 * (s[i] - '0');
-    arr[i] = {w, pc};
-
-    if (hash.find(w) == hash.end()) hash[w] = {0, 0};
-    if (pc == 0)
-      hash[w].first += 1;
-    else
-      hash[w].second += 1;
+    scanf("%I64d", &tmp);
+    arr[i] = {tmp, s[i] - '0'};
+    if (arr[i].second == 1) oya += 1;
   }
   sort(arr.begin(), arr.end());
 
-  int oya_max = 0;
+  int ans = oya;
+  int res = oya;
   rep(i, n) {
-    if (arr[i].second == -1) oya_max += 1;
-    cout << "w:" << arr[i].first << " oyako:" << arr[i].second << endl;
+    // 左から順に動かしていく
+    // 最初は親がすべて正解，子供がすべて不正解
+    // 左に子供を置くたびに＋1
+    if (arr[i].second == 0)
+      res += 1;
+    else
+      res -= 1;
+
+    if (i < n - 1 && arr[i].first != arr[i + 1].first) ans = max(ans, res);
   }
+  ans = max(ans, res);
 
-  int oyako[2] = {0, 0};
-  int ans = 0;
-  rep(i, n) {
-    ll w = arr[i].first;
-    oyako[-1 * arr[i].second] += 1;
-
-    int ok = 0;
-    ok += oyako[0];
-    ok += oya_max - oyako[1];
-    ok -= min(hash[w].first, hash[w].second);
-    if (i == n - 1 || arr[i].first != arr[i + 1].first) ans = max(ans, ok);
-
-    cout << "i:" << i << " w:" << w << " pc:" << arr[i].second << endl;
-  }
   cout << ans << endl;
   return 0;
 }
