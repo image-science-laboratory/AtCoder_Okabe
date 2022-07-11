e33458ce70e60db6a9c93aa88a7ac03752d0116f
beyanon <tokabe333@gmail.com>
2022-07-12 01:39:01 +0900

    create test

---
+++ ABC_250to300/ABC_253/D-FizzBuzzSumHard.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+const double PI = 3.141592653589793;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long int ll;
+using namespace std;
+
+int main() {
+  ll n, a, b, ans = 0;
+  cin >> n >> a >> b;
+
+  cout << pow(10, 9) / 2 * (1 + pow(10, 9)) << endl;
+
+  return 0;
+}
