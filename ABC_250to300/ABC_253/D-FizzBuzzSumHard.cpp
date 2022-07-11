5eb0022e4e585fc46d972f9352e941e2ac6b14c3
beyanon <tokabe333@gmail.com>
2022-07-12 02:04:00 +0900

    Create D

--- ABC_250to300/ABC_253/D-FizzBuzzSumHard.cpp
+++ ABC_250to300/ABC_253/D-FizzBuzzSumHard.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <limits>
 #include <map>
+#include <numeric>
 #include <queue>
 #include <string>
 #include <unordered_map>
@@ -13,11 +14,33 @@ const double PI = 3.141592653589793;
 typedef long long int ll;
 using namespace std;
 
+ll GCD(ll a, ll b) {
+  if (a == 0 || b == 0) return 0;
+  while (a != 0 && b != 0) {
+    if (a > b)
+      a = a % b;
+    else
+      b = b % a;
+  }
+  return max(a, b);
+}
+
+ll LCM(ll a, ll b) {
+  if (a == 0 || b == 0) return 0;
+  return a * b / GCD(a, b);
+}
+
 int main() {
-  ll n, a, b, ans = 0;
+  ll n, a, b;
   cin >> n >> a >> b;
 
-  cout << pow(10, 9) / 2 * (1 + pow(10, 9)) << endl;
+  ll ans = n * (n + 1) / 2;
+  ans -= (n / a) * (2 * a + (n / a - 1) * a) / 2;
+  ans -= (n / b) * (2 * b + (n / b - 1) * b) / 2;
+  ll l = lcm(a, b);
+  ans += (n / l) * (2 * l + (n / l - 1) * l) / 2;
+
+  cout << ans << endl;
 
   return 0;
 }
