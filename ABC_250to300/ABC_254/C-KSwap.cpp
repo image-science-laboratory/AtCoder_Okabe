68eeb87a049cfbc71bacc5d4c8f11deee5b7cc0d
beyanon <tokabe333@gmail.com>
2022-07-11 16:52:54 +0900

    Create C-KSwap.cpp

---
+++ ABC_250to300/ABC_254/C-KSwap.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+const double PI = 3.141592653589793;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long int ll;
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  return 0;
+}
