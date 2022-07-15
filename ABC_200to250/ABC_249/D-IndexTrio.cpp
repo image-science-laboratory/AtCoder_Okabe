7508858ca495e4f9ff1b1467c48066bf33c964c4
souta okabe <tokabe333@gmail.com>
2022-07-15 16:08:59 +0900

    Update D-IndexTrio.cpp

--- ABC_200to250/ABC_249/D-IndexTrio.cpp
+++ ABC_200to250/ABC_249/D-IndexTrio.cpp
@@ -16,61 +16,44 @@ typedef long long int ll;
 using namespace std;
 
 int main() {
-  ll MAX = 100005;
+  ll MAX = 200000;
   ll n;
   cin >> n;
-  unordered_map<ll, bool> hash;
+  ll *arr = new ll[n];
   ll *counts = new ll[MAX]{0};
   rep(i, n) {
     ll tmp;
     scanf("%lld", &tmp);
-    hash[tmp] = true;
+    arr[i] = tmp;
     counts[tmp] += 1;
   }
 
   vector<vector<ll>> eratos(MAX + 1, vector<ll>(0));
-  for (int i = 2; i <= MAX; ++i) {
+  for (int i = 1; i <= MAX; ++i) {
     for (int j = i + i; j <= MAX; j += i) {
       // if (i * i <= j)
       eratos[j].emplace_back(i);
     }
+    eratos[i].emplace_back(i);
   }
 
-  rep(i, 30) {
-    cout << "i:" << i << " ";
-    rep(j, eratos[i].size()) cout << eratos[i][j] << " ";
-    cout << endl;
-  }
+  // rep(i, 30) {
+  //   cout << "i:" << i << " ";
+  //   rep(j, eratos[i].size()) cout << eratos[i][j] << " ";
+  //   cout << endl;
+  // }
 
   ll ans = 0;
-  unordered_map<ll, bool> tansaku;
-  for (auto it1 = hash.begin(); it1 != hash.end(); ++it1) {
-    ll a = it1->first;
-    ll an = counts[a];
-
-    if (counts[1] >= 1) {
-      if (an == 2)
-        ans += 3;
-      else if (an >= 3)
-        ans += an * (an - 1) / 2 * 3;
-    }
-    // ans += counts[1] * an * (an - 1) / 2;
-    cout << "a:" << a << " an" << an << " eratossize:" << eratos[a].size()
-         << " ans:" << ans << endl;
-
+  rep(i, n) {
+    ll a = arr[i];
     rep(j, eratos[a].size()) {
       ll b = eratos[a][j];
       ll bn = counts[b];
+
       ll c = a / b;
       ll cn = counts[c];
-      cout << " b:" << b << " c:" << c << " bn:" << bn << " cn:" << cn;
 
-      if (b == c) {
-        ans += an * bn * (bn - 1) / 2;
-      } else {
-        ans += an * bn * cn * 3;
-      }
-      cout << " ans:" << ans << endl;
+      ans += bn * cn;
     }
   }
   cout << ans << endl;
