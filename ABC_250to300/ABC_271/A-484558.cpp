201dcf0e84f32e57e6bdf0101c87c9d92a92192d
beyanon <tokabe333@gmail.com>
2022-10-06 15:37:22 +0900

    Update : ABC271-A

--- ABC_250to300/ABC_271/A-484558.cpp
+++ ABC_250to300/ABC_271/A-484558.cpp
@@ -17,23 +17,26 @@ using namespace std;
 
 int main()
 {
-    unordered_map<char, int> hash = {
-        {'0', 0},
-        {'1', 1},
-        {'2', 2},
-        {'3', 3},
-        {'4', 4},
-        {'5', 5},
-        {'6', 6},
-        {'7', 7},
-        {'8', 8},
-        {'9', 9},
-        {'A', 10},
-        {'B', 11},
-        {'C', 12},
-        {'D', 13},
-        {'E', 14},
-        {'F', 15}};
+    ll     n, i = 0;
+    string ans = "";
+    cin >> n;
+    while (n > 0) {
+        ll   hoge = n % 16;
+        char c;
+        if (hoge < 10)
+            c = ('0' + hoge);
+        else
+            c = ('A' + (hoge - 10));
+
+        ans = c + ans;
+        n   = n / 16;
+    }
+
+    if (ans.size() == 0)
+        ans = "00" + ans;
+    else if (ans.size() == 1)
+        ans = '0' + ans;
+    cout << ans << endl;
 
     return 0;
 }
