6b88870ba82120f031af3ea002fdf441aafca024
beyanon <tokabe333@gmail.com>
2022-10-06 15:40:24 +0900

    Update : template

--- _CppTemplate.cpp
+++ _CppTemplate.cpp
@@ -10,13 +10,18 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+using namespace std;
 const double PI = 3.141592653589793;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-typedef long long int ll;
-using namespace std;
+typedef long long int              ll;
+typedef vector<ll>                 vll;
+typedef vector<vector<ll>>         vvll;
+typedef vector<vector<vector<ll>>> vvvll;
 
-int main() {
-  int n;
+int main()
+{
+    ll n;
 
-  return 0;
+    return 0;
 }
