30b6f3f91b3106968a6e8e3ac427a9ae5dc605b6
beyanon <tokabe333@gmail.com>
2022-10-07 12:03:31 +0900

    Update : 双方向リストいい感じ

--- ABC_250to300/ABC_271/C-Manga.cpp
+++ ABC_250to300/ABC_271/C-Manga.cpp
@@ -19,103 +19,169 @@ typedef vector<ll>                 vll;
 typedef vector<vector<ll>>         vvll;
 typedef vector<vector<vector<ll>>> vvvll;
 
-class RingBuffer {
-  public:
-    ll  max_size = 100000000; // 10^8
-    ll *buff;
-    ll  top    = 0;
-    ll  bottom = 0;
-    ll  size   = 0;
+/// @brief 双方向キューくん
+/// @tparam 格納するデータ型
+template <class T>
+class DoubleEndedQueue {
+  private:
+    // クラス内で使うノード構造体
+    class Node {
+      public:
+        T     data;
+        Node *next;
+        Node *prev;
 
-    RingBuffer() { buff = new ll[max_size]; }
-    RingBuffer(ll n) {
-        buff     = new ll[n];
-        max_size = n;
-    }
-
-    ll back() { return buff[bottom]; }
-
-    ll front() { return buff[top]; }
-
-    void push_back(ll n) {
-        buff[bottom] = n;
-        bottom       = (bottom + 1) % max_size;
-        if (top == bottom) call_error();
-        size += 1;
-    }
-
-    void push_front(ll n) {
-        buff[top] = n;
-        top       = top > 0 ? top - 1 : max_size - 1;
-        if (top == bottom) call_error();
-        size += 1;
-    }
-
-    ll pop_back() {
-        if (size == 0) call_error();
-
-        bottom = bottom > 0 ? bottom - 1 : max_size - 1;
-        ll ret = buff[bottom];
-        if (bottom < 0) bottom = max_size + bottom;
-        size -= 1;
-        return ret;
-    }
-
-    ll pop_front() {
-        if (size == 0) call_error();
-
-        ll ret = buff[top];
-        top    = (top + 1) % max_size;
-        size -= 1;
-        return ret;
-    }
-
-    void call_error() {
-        if (bottom == top) {
-            cout << "Error : 要素数エラー" << endl;
-            exit(0);
+        Node(T d) {
+            this->data = d;
+            this->next = NULL;
+            this->prev = NULL;
         }
+    };
+
+    // 配列っぽいやつ(前後に入れられるように)
+    Node *head;
+    Node *tail;
+
+  public:
+    // 要素数
+    ll size;
+
+    DoubleEndedQueue() {
+        this->size = 0;
+        this->head = NULL;
+        this->tail = NULL;
     }
+
+    // 先頭要素取り出し
+    T front() {
+        if (size == 0) return NULL;
+        return this->head->data;
+    }
+
+    // 最後の要素取り出し
+    T back() {
+        if (size == 0) return NULL;
+        return this->back->data;
+    }
+
+    // 先頭に要素追加
+    void push_front(T data) {
+        Node *node = new Node(data);
+        // 要素がなければheadとtailをその要素に
+        if (this->size == 0) {
+            this->head = node;
+            this->tail = node;
+            this->size = 1;
+        }
+        // データがあれば先頭だけ変更
+        else {
+            node->next       = this->head;
+            this->head->prev = node;
+            this->head       = node;
+            this->size += 1;
+        }
+    } // end of method
+
+    // 最後に要素追加
+    void push_back(T data) {
+        Node *node = new Node(data);
+
+        // 要素がなければheadとtailをその要素に
+        if (this->size == 0) {
+            this->head = node;
+            this->tail = node;
+            this->size = 1;
+        }
+        // データがあれば最後だけ変更
+        else {
+            node->prev       = this->tail;
+            this->tail->next = node;
+            this->tail       = node;
+            this->size += 1;
+        }
+    } // end of method
+
+    // 先頭の要素を取り出し
+    T pop_front() {
+        // 要素数が0ならぬるぽ
+        if (this->size == 0) {
+            cout << "Error : ぬるぽ" << endl;
+            exit(-1);
+        }
+
+        T ret = this->head->data;
+        // 要素数が1ならheadとtailをNULLにする
+        if (this->size == 1) {
+            free(this->head);
+            this->head = NULL;
+            this->tail = NULL;
+        }
+        // データあれば先頭だけ削除
+        else {
+            this->head = this->head->next;
+            free(this->head->prev);
+        }
+
+        this->size -= 1;
+        return ret;
+    } // end of method
+
+    // 最後の要素を取出し
+    T pop_back() {
+        // 要素数が0ならぬるぽ
+        if (this->size == 0) {
+            cout << "Error : ぬるぽ" << endl;
+            exit(-1);
+        }
+
+        T ret = this->tail->data;
+        // 要素数が1ならheadとtailをNULLにする
+        if (this->size == 1) {
+            free(this->head);
+            this->head = NULL;
+            this->tail = NULL;
+        }
+        // データがあれば最後だけ削除
+        else {
+            this->tail = this->tail->prev;
+            free(this->tail->next);
+        }
+
+        this->size -= 1;
+        return ret;
+    } // end of method
+
+    void show() {
+        if (this->size == 0) {
+            cout << "No Elements" << endl;
+            return;
+        }
+
+        Node *tmp = this->head;
+        cout << "head:" << head << " tail" << tail << endl;
+        cout << "size:" << this->size << endl
+             << "value:";
+        while (true) {
+            cout << tmp->data << " ";
+            if (tmp->next == NULL) break;
+            tmp = tmp->next;
+        }
+
+        cout << endl;
+    } // end of method
 };
 
 int main() {
-    ll n, t;
+    ll n;
     cin >> n;
     vll manga(n);
-
-    RingBuffer rb;
-    rep(i, n) {
-        scanf("%lld", &t);
-        manga[i] = t;
-    }
+    rep(i, n) scanf("%lld", &manga[i]);
     sort(manga.begin(), manga.end());
+
+    DoubleEndedQueue<ll> deque;
     rep(i, n) {
-        rb.push_back(manga[i]);
+        deque.push_back(manga[i]);
     }
 
-    for (int i = 1; true; ++i) {
-        // cout << "top:" << rb.top << " bottom:" << rb.bottom << " ";
-        // for (int j = rb.top; j < rb.bottom; ++j) {
-        //     cout << rb.buff[j] << " ";
-        // }
-        // cout << endl;
-
-        if (rb.size <= 0 || rb.front() != i && rb.size == 1) {
-            cout << i - 1 << endl;
-            return 0;
-        }
-
-        if (rb.front() != i) {
-            rb.pop_back();
-            rb.pop_back();
-        } else {
-            rb.pop_front();
-            while (rb.front() == i) {
-                rb.pop_front();
-                rb.push_back(pow(10, 9) + 5);
-            }
-        }
-    }
-
-    return 0;
+    deque.show();
 }
