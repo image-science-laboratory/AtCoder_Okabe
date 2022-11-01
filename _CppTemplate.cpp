99437d11439ba012d120f75b93187a35ae9d8e44
beyanon <tokabe333@gmail.com>
2022-11-01 21:49:36 +0900

    update : テンプレート更新

--- _CppTemplate.cpp
+++ _CppTemplate.cpp
@@ -16,10 +16,24 @@
 using namespace std;
 const double PI = 3.141592653589793;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-typedef long long int              ll;
-typedef vector<ll>                 vll;
-typedef vector<vector<ll>>         vvll;
-typedef vector<vector<vector<ll>>> vvvll;
+#define mod107(m) m % 1000000007
+#define mod998(m) m % 998244353
+typedef long long int                  ll;
+typedef vector<ll>                     vll;
+typedef vector<vector<ll>>             vvll;
+typedef vector<vector<vector<ll>>>     vvvll;
+typedef vector<int>                    vi;
+typedef vector<vector<int>>            vvi;
+typedef vector<vector<vector<int>>>    vvvi;
+typedef vector<float>                  vf;
+typedef vector<vector<float>>          vvf;
+typedef vector<vector<vector<float>>>  vvvf;
+typedef vector<double>                 vd;
+typedef vector<vector<double>>         vvd;
+typedef vector<vector<vector<double>>> vvvd;
+typedef pair<ll, ll>                   pii;
+typedef pair<ll, string>               pis;
+typedef pair<string, ll>               psi;
 
 int main() {
     ll n;
