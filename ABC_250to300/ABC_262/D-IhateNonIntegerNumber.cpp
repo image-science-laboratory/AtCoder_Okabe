3f9998ca8d2f303572999de96471d3617413dc3e
beyanon <tokabe333@gmail.com>
2022-11-07 23:36:25 +0900

    update : ABC262 Dまで

--- ABC_250to300/ABC_262/D-IhateNonIntegerNumber.cpp
+++ ABC_250to300/ABC_262/D-IhateNonIntegerNumber.cpp
@@ -14,10 +14,13 @@
 #include <vector>
 
 using namespace std;
+
 const double PI = 3.141592653589793;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define mod107(m) m % 1000000007
 #define mod998(m) m % 998244353
+// #define mod998 998244353
+
 typedef long long int                  ll;
 typedef vector<ll>                     vll;
 typedef vector<vector<ll>>             vvll;
@@ -38,25 +41,33 @@ typedef pair<string, ll>               psi;
 int main() {
     ll n;
     cin >> n;
-    vi  arr(n);
-    int hoge;
-    rep(i, n) {
-        scanf("%d", &hoge);
-        arr[i] = hoge - 1;
-    }
-    int    onazi = 0;
-    double ans   = 0;
-    rep(i, n) {
-        if (i == arr[i]) {
-            ans += onazi;
-            onazi += 1;
-            // cout << "i:" << i << " onazi:" << onazi << " ans:" << ans << endl;
-        } else if (arr[arr[i]] == i) {
-            ans += 0.5;
-            // cout << "i:" << i << " ans:" << ans << endl;
+    vll arr(n);
+    rep(i, n) scanf("%lld", &arr[i]);
+
+    ll ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        vvvll dp(n + 1, vvll(i + 1, vll(i, 0)));
+        dp[0][0][0] = 1;
+
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k <= i; ++k) {
+                for (int l = 0; l < i; ++l) {
+                    // 選ばないパターン
+                    dp[j + 1][k][l] = mod998(dp[j + 1][k][l] + dp[j][k][l]);
+                    // 選ぶパターｎ
+                    if (k >= i) continue;
+                    // cout << "i:" << i << " j:" << j << " k:" << k << " l:" << l << endl;
+                    ll hoge = dp[j + 1][k + 1][(l + arr[j]) % i];
+                    // cout << "hoge:" << hoge << "dp:" << dp[j][k][l] << endl;
+                    dp[j + 1][k + 1][(l + arr[j]) % i] = mod998(dp[j][k][l] + hoge);
+                }
+            }
         }
-        // if (i % 1000 == 0) cout << "i:" << i << " ans:" << ans << endl;
+        // cout << " ans:" << ans << endl;
+        ans = ans + mod998(dp[n][i][0]);
     }
-    cout << (ll)ans << endl;
+
+    cout << mod998(ans) << endl;
+
     return 0;
 }
