ca77543a961007166d95ccde37ae02de84a1671b
beyanon <tokabe333@gmail.com>
2022-11-08 23:32:37 +0900

    update : ABC261 Dまで

--- test.cpp
+++ test.cpp
@@ -2,19 +2,8 @@
 #include <functional>
 
 #include <iostream>
-
-// nPnç¸º?½®é¬??¿½è›»åŠ±â†“èŸ‡?½¾ç¸ºåŠ±â€»èœ??½¦é€??¿½ç¹§è²žï½®æº¯?½¡å¾Œâ??ç¹§?¿½
-void foreach_permutation(int n, std::function<void(int *)> f) {
-    int indexes[n];
-    for (int i = 0; i < n; i++)
-        indexes[i] = i;
-    do {
-        f(indexes);
-    } while (std::next_permutation(indexes, indexes + n));
-}
-
+using namespace std;
 int main() {
-    foreach_permutation(3, [](int *indexes) {
-        std::cout << indexes[0] << ',' << indexes[1] << ',' << indexes[2] << std::endl;
-    });
+    int hoge = 10000000000;
+    cout << hoge << endl;
 }
