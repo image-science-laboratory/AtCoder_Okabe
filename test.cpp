9df5ad3e6773161c74316c9d06a120ae97bd1128
beyanon <tokabe333@gmail.com>
2022-11-12 22:43:14 +0900

    add : ABC277 Dのデバッグみすった

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include <algorithm>
 #include <functional>
-
 #include <iostream>
+#include <set>
 using namespace std;
 int main() {
-    int hoge = 10000000000;
-    cout << hoge << endl;
+    std::set<int> st{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    for (auto itr = st.begin(); itr != st.end(); ++itr) {
+        std::cout << *itr << "\n"; // イテレータの指す先のデータを表示
+    }
+
+    auto it = st.find(4);
+    cout << it - st.begin() << endl;
 }
