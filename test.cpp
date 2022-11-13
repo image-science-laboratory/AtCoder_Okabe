8014bb960a5006549ee28e79aada0bc9d44ed3d4
beyanon <tokabe333@gmail.com>
2022-11-13 14:01:07 +0900

    update : ABC268Cまで

--- test.cpp
+++ test.cpp
@@ -4,11 +4,5 @@
 #include <set>
 using namespace std;
 int main() {
-    std::set<int> st{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-    for (auto itr = st.begin(); itr != st.end(); ++itr) {
-        std::cout << *itr << "\n"; // イテレータの指す先のデータを表示
-    }
-
-    auto it = st.find(4);
-    cout << it - st.begin() << endl;
+    cout << (-3) % 4 << endl;
 }
