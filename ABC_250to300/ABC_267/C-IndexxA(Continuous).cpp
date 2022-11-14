313d25179c63434d5179c35ad27b8af6fd89fe48
beyanon <tokabe333@gmail.com>
2022-11-14 12:49:22 +0900

    update : C途中

--- ABC_250to300/ABC_267/C-IndexxA(Continuous).cpp
+++ ABC_250to300/ABC_267/C-IndexxA(Continuous).cpp
@@ -41,49 +41,12 @@ typedef pair<int, int>                 pii;
 typedef pair<int, string>              pis;
 typedef pair<string, int>              psi;
 
-int ctoi(char c) {
-    return c - '0';
-}
-
 int main() {
-    bool   debug = false;
-    string str;
-    cin >> str;
-
-    if (str[0] == '1') {
-        cout << "No" << endl;
-        return 0;
-    }
-
-    vi rows(7, 0);
-    rows[0] = ctoi(str[6]);
-    rows[1] = ctoi(str[3]);
-    rows[2] = ctoi(str[1]) + ctoi(str[7]);
-    rows[3] = ctoi(str[0]) + ctoi(str[4]);
-    rows[4] = ctoi(str[2]) + ctoi(str[8]);
-    rows[5] = ctoi(str[5]);
-    rows[6] = ctoi(str[9]);
-
-    if (debug) {
-        rep(i, 7) cout << rows[i] << " ";
-        cout << endl;
-    }
-
-    int left  = 999;
-    int right = -999;
-    rep(i, 7) {
-        if (rows[i] == 0) continue;
-        left  = min(left, i);
-        right = max(right, i);
-    }
-
-    for (int i = left; i <= right; ++i) {
-        if (rows[i] == 0) {
-            cout << "Yes" << endl;
-            return 0;
-        }
-    }
-    cout << "No" << endl;
+    bool debug = true;
+    ll   n, m;
+    cin >> n >> m;
+    vi  arr(n);
+    int minuses = 0;
 
     return 0;
 }
