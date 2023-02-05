11c1bc4c513dbdaadb6043992c5e0f5c90cbe8ae
tokabe333 <tokabe333@gmail.com>
2023-02-05 13:40:51 +0900

    update : templateを分離

--- _CppTemplate.cpp
+++ _CppTemplate.cpp
@@ -14,40 +14,6 @@
 #include <unordered_map>
 #include <vector>
 
-typedef int XY_Type;
-class XY {
-  public:
-    XY_Type y;
-    XY_Type x;
-
-    XY() {}
-
-    XY(XY_Type y, XY_Type x) {
-        this->y = y;
-        this->x = x;
-    }
-
-    XY operator+(const XY &dst) {
-        return XY(this->y + dst.y, this->x + dst.x);
-    }
-
-    XY operator-(const XY &dst) {
-        return XY(this->y - dst.y, this->x - dst.x);
-    }
-
-    XY operator*(const XY_Type &dst) {
-        return XY(this->y * dst, this->x * dst);
-    }
-
-    XY operator/(const XY_Type &dst) {
-        return XY(this->y / dst, this->x / dst);
-    }
-
-    double norm() {
-        return sqrt(this->y * this->y + this->x * this->x);
-    }
-};
-
 using namespace std;
 const double PI = 3.141592653589793;
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
